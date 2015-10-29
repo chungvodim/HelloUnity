8babc35e3c69dcd07817b69e1b6bf4a1e5db72b7
chungvodim <ba198647@gmail.com>
2015-10-29 19:39:14 +0800

    add script

---
+++ HelloUnity2D/Assets/Rocket.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rocket : MonoBehaviour {
+
+    // Require the rocket to be a rigidbody.
+    // This way we the user can not assign a prefab without rigidbody
+    public Rigidbody rocket;
+    public float speed = 10f;
+
+    void FireRocket()
+    {
+        Rigidbody rocketClone = (Rigidbody)Instantiate(rocket, transform.position, transform.rotation);
+        rocketClone.velocity = transform.forward * speed;
+
+        // You can also acccess other components / scripts of the clone
+        rocketClone.GetComponent<MyRocketScript>().DoSomething();
+    }
+
+    // Calls the fire method when holding down ctrl or mouse
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            FireRocket();
+        }
+    }
+}
